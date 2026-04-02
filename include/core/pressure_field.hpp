8e235fe5e9430431835117d5bc397a870fd4a2a6
Daniel <danikoynov2006@gmail.com>
2026-04-03 00:26:29 +0200

    feat: add pressure field class and tests for it

---
+++ include/core/pressure_field.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+
+namespace cfd {
+
+    class PressureField {
+
+        private:
+            std::size_t width_, height_;
+            const double outside_pressure_;     
+            std::vector<double> p_;
+
+        public:
+
+            PressureField(std::size_t width, std::size_t height, 
+                double outside_pressure);
+            
+            void check_bounds(int i, int j) const;
+
+            double& get_p(int i, int j);
+            const double& get_p(int i, int j) const;
+
+            double read_p_or_outside(int i, int j) const;
+
+    };
+}
---
+++ src/core/pressure_field.cpp
@@ -0,0 +1,39 @@
+#include "core/pressure_field.hpp"
+#include <stdexcept>
+
+namespace cfd {
+
+    PressureField::PressureField(
+        std::size_t width, std::size_t height, 
+        double outside_pressure
+    ): width_(width), height_(height),
+        outside_pressure_(outside_pressure), p_(width * height) {}
+
+    void PressureField::check_bounds(int i, int j) const
+    {
+        if (i < 0 || i >= static_cast<int>(width_) ||
+            j < 0 || j >= static_cast<int>(height_)) {
+            throw std::out_of_range("pressure indexes out of bounds");
+        }
+    }
+
+    double& PressureField::get_p(int i, int j) {
+        check_bounds(i, j);
+        return p_[i * height_ + j];
+    }
+
+    const double& PressureField::get_p(int i, int j) const {
+        check_bounds(i, j);
+        return p_[i * height_ + j];
+    }
+
+    double PressureField::read_p_or_outside(int i, int j) const {
+        if (i < 0 || i >= static_cast<int>(width_) ||
+            j < 0 || j >= static_cast<int>(height_)) 
+            return outside_pressure_;
+        
+        return get_p(i, j);
+    }
+
+
+}
